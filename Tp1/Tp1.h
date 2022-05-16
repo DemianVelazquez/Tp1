bfa290c37f7cf18a921c11ccc0ca724c2cd5ab31
DemianVelazquez <demianV94@gmail.com>
2022-05-16 17:19:43 -0300

    Tp1.h

---
+++ Tp1/Tp1.h
@@ -0,0 +1,25 @@
+#ifndef TP1_H_INCLUDED
+#define TP1_H_INCLUDED
+#define PI 3.1416
+
+//Ejercicio 1
+//float superficeCirculo(int radio)
+//{
+    //return( superficie = PI * radio * radio);
+//}
+
+//ejercicio 2
+long suma(int desde, int hasta)
+{
+    for(int i = desde; i == hasta; ++i)
+        int total = i + hasta;
+    return(total);
+}
+//Ejercicio 9
+//void tablaDel(int n, int *tabla)
+//{
+    //for(int i = 0; i < 10; ++i)
+       // tabla[i] = n * (i + 1);
+//}
+
+#endif // TP1_H_INCLUDED
--- Tp1/main.cpp
+++ Tp1/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include "Tp1.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int desde;
+    int hasta;
+    cout << "ingreses un numero :";
+    cin >> desde;
+    cin.sync();
+    cout << "ingrese otro numero :";
+    cin >> hasta;
+    cin.sync();
+    cout << long suma(int desde, int hasta);
+
+
+
 }
+
+
